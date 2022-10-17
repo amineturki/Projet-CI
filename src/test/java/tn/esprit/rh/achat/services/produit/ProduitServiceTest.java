4eb857e681dbd29fa8e66774fc4bbd40c642b005
amineturki <58289763+amineturki@users.noreply.github.com>
2022-10-17 02:05:00 +0100

    Update ProduitServiceTest.java

--- src/test/java/tn/esprit/rh/achat/services/produit/ProduitServiceTest.java
+++ src/test/java/tn/esprit/rh/achat/services/produit/ProduitServiceTest.java
@@ -1,11 +1,17 @@
 package tn.esprit.rh.achat.services.produit;
 
 import org.junit.jupiter.api.*;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import tn.esprit.rh.achat.entities.Produit;
 import tn.esprit.rh.achat.services.IProduitService;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
+import java.time.LocalDate;
+import java.util.Date;
 import java.util.List;
 
 @SpringBootTest
@@ -22,5 +28,27 @@ public class ProduitServiceTest {
         Assertions.assertEquals(0, listProduits.size());
     }
 
+    
+    @Test
+    @Order(2)
+    public void testAddProduit() {
+        Date date = new Date();
+        Produit p = new Produit("ss","ss",100,date,date);
+    	Produit savedStock= ps.addProduit(p);
+    assertEquals(p.getLibelleProduit(), savedStock.getLibelleProduit());
+    }
+    
+    @Test
+    @Order(3)
+    public void testRetrieveProduit() {
+    	Produit p = ps.retrieveProduit((long) 1);
+    assertEquals("1", p.getIdProduit());
+    }
+    @Test
+    @Order(4)
+    public void testDeleteProduit() {
+    	ps.deleteProduit((long) 1);
+    assertNull(ps.retrieveProduit((long) 1));
+    }
 
 }
