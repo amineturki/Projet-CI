f5e4e0c6194f512f2f315453b78e7ae9f6d26f85
amineturki <58289763+amineturki@users.noreply.github.com>
2022-10-24 01:04:50 +0100

    Update AchatApplication.java

--- src/main/java/tn/esprit/rh/achat/AchatApplication.java
+++ src/main/java/tn/esprit/rh/achat/AchatApplication.java
@@ -3,7 +3,9 @@ package tn.esprit.rh.achat;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+@EnableWebMvc
 @EnableScheduling
 @SpringBootApplication
 public class AchatApplication {
