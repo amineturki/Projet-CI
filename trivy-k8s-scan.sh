ba446d4c3abd5ad9f18670a1fe26173b0a3f5ef4
hazemtahri <73651521+hazemtahri@users.noreply.github.com>
2022-10-25 02:43:12 +0200

    Create trivy-k8s-scan.sh

---
+++ trivy-k8s-scan.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#trivy-k8s-scan
+
+echo hazemtahri/spring-ci:latest #getting Image name from env variable
+
+docker run --rm -v $WORKSPACE:/root/.cache/ aquasec/trivy:0.17.2 -q image --exit-code 0 --severity LOW,MEDIUM,HIGH --light hazemtahri/spring-ci:latest
+docker run --rm -v $WORKSPACE:/root/.cache/ aquasec/trivy:0.17.2 -q image --exit-code 1 --severity CRITICAL --light hazemtahri/spring-ci:latest
+
+    # Trivy scan result processing
+    exit_code=$?
+    echo "Exit Code : $exit_code"
+
+    # Check scan results
+    if [[ ${exit_code} == 1 ]]; then
+        echo "Image scanning failed. Vulnerabilities found"
+        exit 1;
+    else
+        echo "Image scanning passed. No vulnerabilities found"
+    fi;
